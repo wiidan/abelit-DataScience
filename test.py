d909a02fe0a4a3252b198b23cfe0de6313252c3f
abelit <ychenid@live.com>
2020-04-10 18:56:02 +0800

    update

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 import os
+import shutil
 
 os.chdir("./webapp")
 
-os.system("npm run build")
+# os.system("npm run build")
+
+
+shutil.copy('./dist/index.html','../datascience/templates/index.html')
+shutil.copy('./dist/favicon.ico','../datascience/static/favicon.ico')
+# # shutil.copytree('./dist/static/*','../datascience/static/*')
+print(os.listdir('./dist/static'))
+
+for i in os.listdir('./dist/static'):
+    shutil.copytree('./dist/static/'+i,'../datascience/static/'+i)
